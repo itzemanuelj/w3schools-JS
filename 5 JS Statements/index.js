aa0577a9eca5d9e1fa1607295d37ec8aebbbffc2
Emanuel Joness <itzemanuelj@gmail.com>
2023-06-09 22:53:57 -0400

    commit to JavaScript Statements

---
+++ 5 JS Statements/index.js
@@ -0,0 +1,21 @@
+// 5 JS Statements
+let x, y, z;
+x = 1;
+y = 2;
+z = x + y;
+print(z);
+
+// Semicolons ;
+
+let a, b, c;
+a = 1;
+b = 2;
+c = a + b;
+print(c);
+
+a = 1;
+(b = 2), (c = a + b);
+print(c);
+
+//JavaScript White Space
+let person = "justin";
