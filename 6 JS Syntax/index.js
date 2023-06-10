a912b06ab0d6674abfa47f21d73b28af487efbc5
Emanuel Joness <itzemanuelj@gmail.com>
2023-06-09 23:07:29 -0400

    commit to JavaScript Syntax in JS

---
+++ 6 JS Syntax/index.js
@@ -0,0 +1,70 @@
+/// JavaScript Syntax
+
+/// How to create variables:
+let x;
+let y;
+
+/// How to use variables:
+x = 1;
+y = 2;
+let z = x + y;
+
+/// JavaScript Literals
+/// 1. Numbers are written with or without decimals:
+print(100);
+print(100.1);
+
+/// 2. Strings are text, written within double or single quotes:
+print("justin");
+print("justin");
+
+///JavaScript Variables
+
+var name;
+let name;
+const fname = "emanuel";
+
+/// JavaScript Operators
+
+print(5 + 5 + 10);
+
+///JavaScript uses an assignment operator ( = ) to assign values to variables:
+let a, b;
+a = 1;
+b = 2;
+print(a + b);
+
+/// JavaScript Expressions
+print(6 * 1);
+print("emanuel" + "" + "jones");
+
+/// JavaScript Keywords
+let name = "emanuel";
+
+var lname = "jones";
+
+const number = 1;
+
+/// JavaScript Comments
+
+/// hello world
+// Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing. It's also called placeholder or filler text. It's a convenient tool for mock-ups.
+
+/// JavaScript Identifiers / Names
+/// A letter (A-Z or a-z)
+/// A dollar sign ($)
+/// Or an underscore (_)
+
+///JavaScript is Case Sensitive
+
+let lastName, firstName;
+lastName = "jones";
+firstName = "emanuel";
+
+/// JavaScript and Camel Case
+timeRange = 1;
+helloWorldOne = "hello world one";
+
+/// JavaScript Character Set
+
+/// JavaScript uses the Unicode character set.
