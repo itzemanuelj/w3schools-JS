0b00fccb6ffbea9839c33c08140f7973d3a46fd2
Emanuel Joness <itzemanuelj@gmail.com>
2023-06-10 23:08:30 -0400

    commits to JavaScript Variables

---
+++ 8 JS Variables/index.js
@@ -0,0 +1,102 @@
+/// JS Variables
+
+/// Automatically
+x = 5;
+y = 5;
+z = x + y;
+print(z);
+
+/// Example using var
+
+var x = 5;
+var y = 5;
+var z = x + y;
+print(z);
+
+/// Example using let
+
+let x = 5;
+let y = 5;
+let z = x + y;
+print(z);
+
+/// Example using const
+
+const x = 5;
+const y = 5;
+const z = x + y;
+print(z);
+
+/// Mixed Example
+
+const price1 = 1;
+const price2 = 1;
+let total = price1 + price2;
+print(total);
+
+/// Just like in algebra, variables hold values:
+
+let x = 5;
+let y = 5;
+
+let z = x + y;
+
+/// The Assignment Operator =
+
+x = x + 5;
+
+/// JavaScript Data Types
+
+const pi = 3.14;
+let person = "Justin";
+let awnser = true;
+
+print(type(pi));
+print(type(person));
+print(type(awnser));
+
+/// Declaring a JavaScript Variable
+
+fname = "emanuel";
+var fname = "emanuel";
+let fname = "emanuel";
+
+/// One Statement, Many Variables
+
+let person1 = "justin",
+  carName = "tesla",
+  price = 200;
+
+/// A declaration can span multiple lines:
+// let person1 = "justin",
+// carName = "tesla",
+// price = 200;
+
+/// Value = undefined
+
+let tesla;
+
+/// Re-Declaring JavaScript Variables
+
+let lname = "jones";
+lname;
+print(lname);
+print(type(lname));
+
+/// JavaScript Arithmetic
+
+let x = 5 + 1 + 1;
+
+/// You can also add strings, but strings will be concatenated:
+
+let x = "john" + "" + "boikins";
+
+myName = "emanuel";
+car = "tesla";
+year = 2023;
+
+console.log(`my name is ${myName} i own a ${car}
+model year is ${year}`);
+
+/// JavaScript Underscore (_)
+let _lname = "emanuel";
