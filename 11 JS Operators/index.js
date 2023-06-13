3088ba410212a5b4b9f49aea315f838bb71ca09a
Emanuel Joness <itzemanuelj@gmail.com>
2023-06-12 22:13:36 -0400

    commits to JavaScript Operators

---
+++ 11 JS Operators/index.js
@@ -0,0 +1,57 @@
+/// JavaScript Operators
+
+/// Assignment Examples
+
+let a = 1;
+
+/// JavaScript Addition
+
+let b = 2;
+
+let c = a + b;
+console.log(c);
+
+/// JavaScript Multiplication
+
+let d = 1;
+let e = 2;
+let f = d * e;
+console.log(f);
+
+// Types of JavaScript Operators
+
+/// JavaScript Arithmetic Operators
+
+let g = 1;
+let h = (100 + 50) * g;
+console.log(h);
+
+/// JavaScript Assignment Operators
+let x = 10;
+x += 5;
+
+/// JavaScript String Comparison
+let text1 = 1;
+let text2 = 2;
+let result = text1 > text2;
+
+/// JavaScript String Addition
+
+let name = "justin";
+let lname = "jones";
+let fullName = name + "" + lname;
+console.log(fullName);
+
+/// Adding Strings and Numbers
+
+let xx = 5 + 5;
+let yy = "5" + 5;
+let zz = "hello" + 5;
+//10
+//55
+//Hello5
+
+/// JavaScript Logical Operators
+// && and
+// || or
+// ! not
