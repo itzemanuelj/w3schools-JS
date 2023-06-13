55c2a59005f0ff63b9fcd91536920ef04ab1a532
Emanuel Joness <itzemanuelj@gmail.com>
2023-06-12 21:53:26 -0400

    commit to JavaScript Const

---
+++ 10 JS Const/index.js
@@ -0,0 +1,29 @@
+/// JavaScript Const
+
+/// Cannot be Reassigned
+
+const pi = 3.14;
+console.log(pi);
+
+/// Must be Assigned
+
+// const pi;
+
+/// Constant Arrays
+
+const cars = ["tesla", "charger", "challanger"];
+cars.push("audi"); // you can add elements but cannt change it
+
+/// cars[0] = 'ram'; not allowed
+
+/// Constant Objects
+
+const person = {
+  name: "justin",
+  age: "28",
+  year: 1995,
+};
+
+/// Hoisting
+tesla = "model3";
+tesla;
