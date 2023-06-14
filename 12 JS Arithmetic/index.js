341d83a1eda872accc0fed3c6b45f71dfb8ea3bf
Emanuel Joness <itzemanuelj@gmail.com>
2023-06-14 01:14:38 -0400

    commit to Arithmetic in javascript

---
+++ 12 JS Arithmetic/index.js
@@ -0,0 +1,52 @@
+/// JavaScript Arithmetic
+
+let x = 1 + 50;
+
+let xx = a + b;
+
+/// Adding
+
+let a = 1;
+let b = 2;
+let c = a + b;
+console.log(c);
+
+/// Subtracting
+
+let d = 1;
+let e = 1;
+let f = d - e;
+console.log(f);
+
+/// Multiplying
+
+let g = 1;
+let h = 2;
+let i = g * h;
+console.log(i);
+
+/// Dividing
+let j = 1;
+let k = 2;
+let l = j / k;
+console.log(k);
+
+/// Remainder
+let n = 1;
+let o = 1;
+let p = n % o;
+console.log(p);
+
+/// Incrementing
+let aa = 1;
+aa++;
+let bb = aa;
+console.log(bb);
+
+/// Decrementing
+let dd = 1;
+dd--;
+let ee = dd;
+console.log(ee);
+
+//PEMDAS
