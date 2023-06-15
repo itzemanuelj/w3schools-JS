8f4a12fc316df7b8f0cacd8fd884416ad53f8c8a
Emanuel Joness <itzemanuelj@gmail.com>
2023-06-14 23:28:01 -0400

    commoits to JS assigigment operators

---
+++ 13 JS Assignment/script.js
@@ -0,0 +1,78 @@
+/// Javascript Assignment
+
+/// The = Operator
+
+let a = 10;
+let aa = 10 + a;
+console.log(aa);
+
+/// The += Operator
+let x = 10;
+x += 5;
+console.log(x);
+
+/// The -= Operator
+let b = 10;
+b -= 5;
+console.log(b);
+
+/// The *= Operator
+let c = 10;
+c *= 2;
+console.log(c);
+
+/// The **= Operator power of
+let d = 10;
+d **= 1;
+console.log(d);
+
+/// The /= Operator divides a variable
+let e = 10;
+e /= 1;
+console.log(e);
+
+/// The %= Operator modolus mod
+let f = 10;
+f %= 1;
+console.log(f);
+
+/// The <<= Operator Left Shift Assignment Operator
+let i = 10;
+i <<= 10;
+console.log(i);
+
+/// The >>= Operator Right Shift Assignment Operator
+let j = 10;
+j >>= 10;
+console.log(j);
+
+/// The >>>= Operator
+let k = 10;
+k >>>= 10;
+console.log(k);
+
+/// The |= Operator
+let m = 10;
+m |= 5;
+console.log(m);
+
+///The ^= Operator Bitwise XOR Assignment Operato
+let h = 10;
+h ^= 1;
+console.log(h);
+
+/// The &&= Operator and operator
+let w = 10;
+w &&= 5;
+console.log(w);
+
+/// The ||= Operator  or  operator
+let y = 10;
+y ||= 5;
+console.log(y);
+
+/// The ??= Operator Nullish Coalescing
+
+let z = 10;
+z ??= 5;
+console.log(z);
