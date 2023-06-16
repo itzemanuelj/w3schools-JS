79d9110438a779bab301f303ce48fe1af1f4b1d0
Emanuel Joness <itzemanuelj@gmail.com>
2023-06-15 22:16:50 -0400

    commit to javascript function

---
+++ 15 JavaScript Functions/script.js
@@ -0,0 +1,34 @@
+/// JavaScript Functions
+function myFunction(p1, p2) {
+  return p1 + p2;
+}
+/// JavaScript Function Syntax
+function name(param1, param2, param3) {
+  ///excutue code
+}
+
+let x = a(4, 1);
+
+function a(a, b) {
+  return a * b;
+}
+a();
+
+/// Invoke (call) a function that converts from Fahrenheit to Celsius:
+function toCeli(f) {
+  return (5 / 9) * (f - 32);
+}
+let value = toCeli(77);
+
+/// Local Variables
+function myCar() {
+  let carName = "Telsa Model 3";
+  ///can use carName here
+}
+
+///cannot be used here
+
+function myFunc() {
+  alert("Hello World!");
+}
+myFunc();
