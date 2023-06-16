2a8823ac2194939d5502a4d104d43f30f0cbe7d3
Emanuel Joness <itzemanuelj@gmail.com>
2023-06-15 22:01:58 -0400

    commit to Js data types

---
+++ 14 JS Data Types/script.js
@@ -0,0 +1,64 @@
+///  JS Data Types
+
+/// JavaScript Types are Dynamic
+
+// let x;
+let x = "one";
+x = 1;
+
+/// JavaScript Strings
+let name = "emanuel";
+let fname = "justin";
+console.log(name);
+console.log(fname);
+
+/// JavaScript Numbers
+let aan = 1.2;
+
+/// Exponential Notation
+let b = 12e1;
+let bb = 12e-5;
+
+/// JavaScript BigInt
+let c = bigInt(123456789012345678901234567890);
+console.log(c);
+
+/// JavaScript Booleans
+let isOnline = true;
+let isOffLine = false;
+
+/// JavaScript Arrays
+const cars = ["saab", "tesla", "charger"];
+
+/// JavaScript Objects
+
+const person = {
+  name: "jusitn",
+  lastname: "jones",
+  age: 20,
+};
+
+/// The typeof Operator
+let number = 1;
+let age = 20;
+let names = "hello world!";
+console.log(typeof number);
+console.log(typeof age);
+
+/// Undefined
+var title;
+
+/// Empty Values
+let named = "";
+
+/// Exercise:
+/// Use comments to describe the correct data type of the following variables:
+
+let length = 16; ///number
+
+let lastName = "Johnson"; ///string
+
+const xx = {
+  firstName: "John",
+  lastName: "Doe",
+}; ///object
