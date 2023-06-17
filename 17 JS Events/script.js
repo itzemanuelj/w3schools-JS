d3964fc9ca8ba8b822b42ac8da48ae428c0b6bf6
Emanuel Joness <itzemanuelj@gmail.com>
2023-06-16 23:53:18 -0400

    commit to JS Events

---
+++ 17 JS Events/script.js
@@ -0,0 +1,18 @@
+document.getElementById("fname").onchange = function () {
+  upperCase();
+};
+function upperCase() {
+  var x =
+    (document.getElementById("fname").onchnage =
+    x.value =
+      x.value.toLowerCase());
+}
+
+document.getElementById("fname").onchange = function () {
+  myFunction();
+};
+
+function myFunction() {
+  var x = document.getElementById("fname");
+  x.value = x.value.toUpperCase();
+}
