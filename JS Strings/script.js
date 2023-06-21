ed345cae0cfb6db430433c97ba210e30721dcc34
Emanuel Joness <itzemanuelj@gmail.com>
2023-06-20 21:16:38 -0400

    commit to JavaScript Strings

---
+++ JS Strings/script.js
@@ -0,0 +1,16 @@
+/// JavaScript Strings
+let text = "john doe";
+
+let carName1 = "tesla model 3";
+let carName2 = "tesla model roadster";
+
+let text1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+console.log(length.text1);
+
+/// Escape Character
+let sentec = "hello 'world";
+
+/// JavaScript Strings as Objects
+let name = "emanuel";
+
+let newName = new String("emanuel");
