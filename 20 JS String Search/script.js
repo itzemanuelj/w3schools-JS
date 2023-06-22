4bdf1d1cf2336bf6db3167eb971e6ece9b0371cd
Emanuel Joness <itzemanuelj@gmail.com>
2023-06-22 17:18:17 -0400

    commit to w3 schools  JavaScript String Search in js

---
+++ 20 JS String Search/script.js
@@ -0,0 +1,29 @@
+/// JavaScript String Search
+
+/// JavaScript String indexOf()
+let text = "Please locate where 'locate' occurs!";
+let index = text.indexOf("locate");
+
+/// JavaScript String lastIndexOf()
+let text1 = "Please locate where 'locate' occurs!";
+let lastIndexOf = text1.lastIndexOf("locate");
+
+/// JavaScript String search()
+let text2 = "Please locate where 'locate' occurs!";
+let strSearch = text2.search("locate");
+
+/// JavaScript String match()
+let text3 = "The rain in SPAIN stays mainly in the plain";
+text3.match("ain");
+
+/// JavaScript String includes()
+let text4 = "Hello world, welcome to the universe.";
+text4.includes("world");
+
+/// JavaScript String startsWith()
+let text5 = "Hello world, welcome to the universe.";
+text5.startsWith("Hello");
+
+///JavaScript String endsWith()
+let fname = "John Doe";
+fname.endsWith("Doe");
