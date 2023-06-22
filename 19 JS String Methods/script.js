51435bc8b36c0fc98465959a2f37e7ac294b4526
Emanuel Joness <itzemanuelj@gmail.com>
2023-06-22 00:43:35 -0400

    commit to w3schools string methods in js

---
+++ 19 JS String Methods/script.js
@@ -0,0 +1,26 @@
+/// String Methods
+/// JavaScript String Length
+
+let text = "abcdefghiijklmnopqrstuvwxyz";
+let textLenght = text.length;
+console.log(textLenght);
+
+/// JavaScript String slice()
+let arr = "Apple, Banana, Kiwi";
+let part = arr.slice(7, 13);
+console.log(part);
+//Banana
+
+//// Replacing String Content
+let fname = "emanuel";
+let newName = fname.replace("justin");
+console.log(newName);
+
+/// Converting to Upper and Lower Case
+let text1 = "hello world";
+let text2 = text1.toLocaleUpperCase();
+console.log(text2);
+
+let text3 = "HELLO WORLD";
+let text4 = text3.toLowerCase();
+console.log(text4);
