91cbb52e7237576df724a033114296d626cc2328
Emanuel Joness <itzemanuelj@gmail.com>
2023-06-23 00:08:17 -0400

    commit to w3schools JavaScript Template Literals in js

---
+++ 21 JS Template Literals/script.js
@@ -0,0 +1,22 @@
+///JavaScript Template Literals
+
+/// Back-Tics Syntax
+let a = `hello world!`;
+
+/// Quotes Inside Strings
+let b = `hello my name is 'justin'`;
+
+///Multiline Strings
+let multi = `multi line
+strings
+`;
+
+///Interpolation
+///Variable Substitutions
+let fnaame = "emanuel";
+let lastName = "jones";
+
+let entireName = `${fnaame} + ${lastName}`;
+console.log(entireName);
+
+/// HTML Templates
