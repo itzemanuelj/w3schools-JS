47023fe6a471d2f22819e3b14f836fc0f99a0bdd
Emanuel Joness <itzemanuelj@gmail.com>
2023-06-24 02:01:10 -0400

    commit to w3schools bigInt in js

---
+++ 23 JS BigInt/script.js
@@ -0,0 +1,12 @@
+///JavaScript BigInt
+//JavaScript Integer Accuracy
+let x = 999999999999999;
+let y = 9999999999999999;
+
+///How to Create a BigInt
+let xx = 999999999999999;
+let yy = BigInt(123456789012345);
+
+/// BigInt: A new JavaScript Datatype
+let type = typeof xx;
+console.log(xx);
